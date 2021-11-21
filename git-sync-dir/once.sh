1bffd2a029dafdc5b4c1b5fcf447fee0dfb8c2bb
losfair <zhy20000919@hotmail.com>
2021-11-21 05:21:36 +0000

    Hard reset if merge fails

--- git-sync-dir/once.sh
+++ git-sync-dir/once.sh
@@ -15,5 +15,14 @@ git fetch sync_upstream
 now=$(date -u +"%Y-%m-%d %H-%M-%S")
 git add -A .
 git commit -m "Update $now" || true
+set +e
 git merge --strategy-option theirs sync_upstream/"$GIT_BRANCH" --allow-unrelated-histories -m "Merge $now"
-git push sync_upstream "HEAD:$GIT_BRANCH"
+retval="$?"
+set -e
+if [ "$retval" != "0" ]; then
+  echo "Merge failed - hard resetting to remote version"
+  git reset --hard sync_upstream/"$GIT_BRANCH"
+else
+  echo "Pushing to remote."
+  git push sync_upstream "HEAD:$GIT_BRANCH"
+fi
